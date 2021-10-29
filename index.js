f67957653e28eb67c8236358c60d7b367361f283
Moibe <moi_estrello@hotmail.com>
2021-10-29 00:33:20 -0500

    Entrega 4 - Doble Estado
    - Cambios mayores, se agregó la posibilidad de tener dos estados en la página.
    - La barra de progreso ya no desaparece al terminar.
    - Se agregó Título.
    - Se quitó el 4to campo sobrante.
    - Se agregó una variable para que yo sepa en que Estado está la página.

--- index.js
+++ index.js
@@ -5,28 +5,83 @@ const myInputOne = (e) => {
 const myInputTwo = (e) => {
     document.getElementById('lastnameText').innerHTML = e.value;
 }
-const myInputThree = (e) => {
-    document.getElementById('cityText').innerHTML = e.value;
-}
 const myInputFour = (e) => {
     document.getElementById('numberText').innerHTML = e.value;
 }
 
+
+const optionButton=(e)=>{
+if(e.classList.contains('optionOne')){
+    e.classList.remove('optionOne')
+    e.classList.add('optionTwo')
+    e.innerHTML=`Option B`;
+    formText.innerHTML=`My Form Version B`
+    body.classList.remove('gradient')
+    body.classList.add('gradient1')
+    // $('.growing_bar').css('background','red')
+    $('#adddedSuccessfull').css('display', 'none')
+    $('#barid').css('visibility', 'hidden')
+}
+else{
+    e.classList.remove('optionTwo');
+    e.classList.add('optionOne');
+    e.innerHTML=`Option A`;
+    formText.innerHTML=`My Form Version A`
+    body.classList.remove('gradient1')
+    body.classList.add('gradient')
+    $('#adddedSuccessfull').css('display', 'none')
+    $('#barid').css('visibility', 'hidden')
+    // $('.growing_bar').css('background','rgba(236, 0, 140, .6)')
+}
+}
 const myClick = (e) => {
-    if (nameOne.value >= '0' && (lastnameOne.value >= '0') && (cityOne.value >= '0') && (numberOne.value >= '0')) {
+
+    // if()
+    if(buttonOption.classList.contains('optionOne')){
 
         $('#barid').css('visibility', 'visible')
+        $('.growing-bar').removeClass('growing_barB')
         $('.growing-bar').addClass('growing_bar')
         e.setAttribute("disabled", "false")
-
+    
         setTimeout(function () {
-            $('#barid').css('visibility', 'hidden')
+            // $('#barid').css('visibility', 'hidden')
         }, 13000)
         setTimeout(function () {
             $('#adddedSuccessfull').css('display', 'block')
-        }, 14000)
-    } else {
-        alert('fill inputs ')
+            e.removeAttribute("disabled", "true")
+            nameOne.value='';
+            lastnameOne.value ='';
+            numberOne.value='';
+    
+        }, 13000)
     }
+    else{
+        alert(true)
+        
+        $('#barid').css('visibility', 'visible')
+        $('.growing-bar').removeClass('growing_bar')
+        // $('.growing-bar').css('animation-play-state','play')
+        $('.growing-bar').addClass('growing_barB')
+        e.setAttribute("disabled", "false")
+    
+        setTimeout(function () {
+            // $('#barid').css('visibility', 'hidden')
+        }, 13000)
+        setTimeout(function () {
+            $('#adddedSuccessfull').css('display', 'block')
+            e.removeAttribute("disabled", "true")
+            nameOne.value='';
+            lastnameOne.value ='';
+            numberOne.value='';
+    
+        }, 13000)
+    }
+    // if (nameOne.value >= '0' && (lastnameOne.value >= '0') && (numberOne.value >= '0')) {
+        
+    // }
+    //  else {
+    //     alert('fill inputs ')
+    // }
     // e.preventDefault()
 }
