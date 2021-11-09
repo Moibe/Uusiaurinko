fa3e1aa2e8796766d845dea24582823824f25764
Moibe <moi_estrello@hotmail.com>
2021-11-09 02:02:55 -0600

    Process Ready
    Ahora puede recibir procesos y la barra de progreso no está delimitada por tiempo. Excelente trabajo de Zahid.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // use this variable to set the timing of progress bar
-let timing = 20;
+let timing = 5;
 const myInputOne = (e) => {
     document.getElementById('nameText').innerHTML = e.value;
     glass2.style.opacity = 1;
     glass2.classList.add('animate__fadeInRight')
     if (field1.value >= '0' && (field2.value >= '0') && (field3.value >= '0')) {
-        $('.confetti__button').removeAttr('disabled','');
-        $('.confetti__button').css('cursor','default');
-        $('.confetti__button').css('cursor','pointer');
-        $('.button__text').css('background','#3867d6');
+        $('.confetti__button').removeAttr('disabled', '');
+        $('.confetti__button').css('cursor', 'default');
+        $('.confetti__button').css('cursor', 'pointer');
+        $('.button__text').css('background', '#3867d6');
     }
-    else{
-        $('.confetti__button').attr('disabled','false')
-        $('.confetti__button').css('cursor','default');
-            $('.button__text').css('background','grey');
+    else {
+        $('.confetti__button').attr('disabled', 'false')
+        $('.confetti__button').css('cursor', 'default');
+        $('.button__text').css('background', 'grey');
     }
 }
 
@@ -22,14 +22,14 @@ const myInputTwo = (e) => {
     glass2.style.opacity = 1;
     glass2.classList.add('animate__fadeInRight')
     if (field1.value >= '0' && (field2.value >= '0') && (field3.value >= '0')) {
-        $('.confetti__button').removeAttr('disabled','');
-        $('.confetti__button').css('cursor','pointer');
-        $('.button__text').css('background','#3867d6');
+        $('.confetti__button').removeAttr('disabled', '');
+        $('.confetti__button').css('cursor', 'pointer');
+        $('.button__text').css('background', '#3867d6');
     }
-    else{
-        $('.confetti__button').attr('disabled','false')
-        $('.confetti__button').css('cursor','default');
-            $('.button__text').css('background','grey');
+    else {
+        $('.confetti__button').attr('disabled', 'false')
+        $('.confetti__button').css('cursor', 'default');
+        $('.button__text').css('background', 'grey');
     }
 }
 const myInputFour = (e) => {
@@ -37,14 +37,14 @@ const myInputFour = (e) => {
     glass2.style.opacity = 1;
     glass2.classList.add('animate__fadeInRight')
     if (field1.value >= '0' && (field2.value >= '0') && (field3.value >= '0')) {
-        $('.confetti__button').removeAttr('disabled','');
-        $('.button__text').css('background','#3867d6');
-        $('.confetti__button').css('cursor','pointer');
+        $('.confetti__button').removeAttr('disabled', '');
+        $('.button__text').css('background', '#3867d6');
+        $('.confetti__button').css('cursor', 'pointer');
     }
-    else{
-        $('.confetti__button').attr('disabled','false')
-        $('.confetti__button').css('cursor','default');
-            $('.button__text').css('background','grey');
+    else {
+        $('.confetti__button').attr('disabled', 'false')
+        $('.confetti__button').css('cursor', 'default');
+        $('.button__text').css('background', 'grey');
     }
 }
 const optionButton = (e) => {
@@ -61,8 +61,8 @@ const optionButton = (e) => {
         $('.growing-bar').addClass('growingBarAnimation')
         glass2.classList.remove('animate__fadeInRight')
         glass2.classList.remove('animate__fadeIn')
-        $('.confetti__button').attr('disabled','false');
-        $('.confetti__button').css('cursor','default');
+        $('.confetti__button').attr('disabled', 'false');
+        $('.confetti__button').css('cursor', 'default');
     }
     else {
         e.classList.remove('optionTwo');
@@ -77,19 +77,22 @@ const optionButton = (e) => {
         $('.growing-bar').addClass('growing_barA')
         glass2.classList.remove('animate__fadeInRight')
         glass2.classList.remove('animate__fadeIn')
-        $('.confetti__button').attr('disabled','false')
-        $('.confetti__button').css('cursor','default');
+        $('.confetti__button').attr('disabled', 'false')
+        $('.confetti__button').css('cursor', 'default');
     }
 }
+
+// submit functionality after this
 const myClick = (e) => {
 
+    // check if all the fields are filled
     if (field1.value >= '0' && (field2.value >= '0') && (field3.value >= '0')) {
         glass1.classList.add('animate__fadeOut');
         $('#glass1').css('opacity', '0');
         optionDIv.classList.add('buttonDIvOptoveride')
         $('.button__text').css('background-color', '#3867d6')
-        $('.confetti__button').attr('disabled','true')
-        $('.confetti__button').css('cursor','default');
+        $('.confetti__button').attr('disabled', 'true')
+        $('.confetti__button').css('cursor', 'default');
         $('.button__text').css('background-color', 'grey')
         // console.log(e);
         setTimeout(function () {
@@ -101,72 +104,75 @@ const myClick = (e) => {
     else {
         e.setAttribute("disabled", "")
         $('.button__text').css('background-color', 'grey')
-        // e.setAttribute("disabled", "false")
     }
-    if (buttonOption.classList.contains('optionOne')) {
-        $('#barid').css('visibility', 'visible')
-        $('.growing-bar').removeClass('growing_barB')
-        $('.growing-bar').addClass('growing_bar')
-        // $('.growing-bar').css('animation',`${timing}s linear 0s 1 normal none running fill`);
-        $('.growing-bar').addClass('growing_barA')
-        $('.button-center').css('display','none');
-        $('.confetti__button').attr('disabled','false')
-        $('.confetti__button').css('cursor','default');
-        $('.button__text').css('background','grey');
-        e.setAttribute("disabled", "false")
-        setTimeout(function () {
-            $('#adddedSuccessfull').css('display', 'block')
-            $('.growing-bar').css('animation', 'stop')
-            $('#clickReturn').css('display','block');
-            field1.value = '';
-            field2.value = '';
-            field3.value = '';
-        }, `${timing}000`)
-        
-    }
-    //indeterminateAnimation 1s infinite linear;
-    else {
-        $('#barid').css('visibility', 'visible')
-        $('.growing-bar').removeClass('growing_bar')
-        $('.growing-bar').removeClass('growing_barA')
-        $('.button-center').css('display','none');
-        $('.confetti__button').attr('disabled','false')
-        $('.confetti__button').css('cursor','default');
-        $('.button__text').css('background','grey');
-        // $('.growing-bar').css('animation',`${timing}s linear 0s 1 normal none running fill`);
-        // e.setAttribute("disabled", "false")
 
-        setTimeout(function () {
-        }, `${timing}000`)
-        setTimeout(function () {
-            $('#adddedSuccessfull').css('display', 'block')
-            $('.growing-bar').css('animation', 'asad')
-            $('#clickReturn').css('display','block');
-            field1.value = '';
-            field2.value = '';
-            field3.value = '';
-        }, `${timing}000`)
+    if (buttonOption.classList.contains('optionOne')) {
+        handleOptionOne();
+
+        // call the function to simulate the long running process
+        fakeProcess();
+    }
+    else {
+        handleOptionTwo();
+
+        // call the function to simulate the long running process
+        fakeProcess();
+    }
+
+
+    function handleOptionOne() {
+        $('#barid').css('visibility', 'visible');
+        $('.growing-bar').removeClass('growing_barB');
+        $('.growing-bar').addClass('growing_bar');
+        // $('.growing-bar').css('animation',`${timing}s linear 0s 1 normal none running fill`);
+        $('.growing-bar').addClass('growing_barA');
+        $('.button-center').css('display', 'none');
+        $('.confetti__button').attr('disabled', 'false');
+        $('.confetti__button').css('cursor', 'default');
+        $('.button__text').css('background', 'grey');
+        // e.setAttribute("disabled", "false")
+    }
+
+    function handleOptionTwo() {
+        //indeterminateAnimation 1s infinite linear;
+        $('#barid').css('visibility', 'visible');
+        $('.growing-bar').removeClass('growing_bar');
+        $('.growing-bar').removeClass('growing_barA');
+        $('.button-center').css('display', 'none');
+        $('.confetti__button').attr('disabled', 'false');
+        $('.confetti__button').css('cursor', 'default');
+        $('.button__text').css('background', 'grey');
+        // $('.growing-bar').css('animation',`${timing}s linear 0s 1 normal none running fill`);
+        // e.setAttribute("disabled", "false")
     }
 }
 
-const clickToReturn=(e)=>{
-    // $('#glass1').css('opacity','1');
-    // $('#glass1').removeClass('animate__fadeOut');
-    // $('#glass2').css('opacity','0');
-    // $('.button-center').css('display','block');
-    // $('#barid').css('visibility','hidden')
-    // $('#glass2').css('position', 'relative')
-    // $('#adddedSuccessfull').css('display', 'none')
-    // $('#clickReturn').css('display','none');
-    // glass2.classList.remove('animate__fadeInRight')
-    // glass2.classList.remove('animate__fadeInRight1')
-    // document.getElementById('nameText').innerHTML = ``;
-    // document.getElementById('lastnameText').innerHTML = ``;
-    // document.getElementById('numberText').innerHTML = ``;
-    // $('.growing-bar').css('animation','')
-
+// reset the form
+const clickToReturn = (e) => {
     location.reload();
 }
-const stopProgressBar = () => {
-    $('.growing-bar').css('animation', '')
+
+// use this function to stop the progress bar
+function stopProgressBar() {
+    $('.growing-bar').css('animation', 'asad');
+    $('#clickReturn').css('display', 'block');
+    field1.value = '';
+    field2.value = '';
+    field3.value = '';
+}
+
+// long running process goes here
+function fakeProcess() {
+    let x = "Hello world! Done!!";
+
+    // set fake timeout to simulate a long process
+    // when the process is done, show the success message
+    // and hide the progress bar
+    setTimeout(function () {
+        console.log("Timeout of 5 seconds...");
+
+        $('#adddedSuccessfull').html(x);
+        $('#adddedSuccessfull').css('display', 'block');
+        stopProgressBar();
+    }, 5000);
 }
